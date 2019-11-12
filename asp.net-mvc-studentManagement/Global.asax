7975b7b3d3210e563d65dc021e172d4e9050872a
Vince Harresh Rodriguez <vincer@ccknetwork.local>
2019-11-12 17:54:36 +0800

    Add project files.

---
+++ asp.net-mvc-studentManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp.net_mvc_studentManagement.MvcApplication" Language="C#" %>
---
+++ asp.net-mvc-studentManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_mvc_studentManagement.Startup))]
+namespace asp.net_mvc_studentManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
