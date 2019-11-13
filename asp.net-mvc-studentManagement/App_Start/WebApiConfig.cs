6cba5888bad5b0cb9e766359fa4f2463aaafacd4
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2019-11-13 11:27:17 +0800

    Initial

---
+++ asp.net-mvc-studentManagement/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace asp.net_mvc_studentManagement
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ asp.net-mvc-studentManagement/ViewModel/StudentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using asp.net_mvc_studentManagement.Models;
+namespace asp.net_mvc_studentManagement.ViewModel
+{
+    public class StudentViewModel
+    {
+        public List<StudentModel> Students { get; set; }
+    }
+}
